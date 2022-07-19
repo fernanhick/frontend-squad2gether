e749b6d8d33bb8dc0b7f1d99210f9eed40ece76d
Fernando Hickman <fernanhick@gmail.com>
2022-07-19 22:01:11 +0100

    Users Componets List ToDo

--- src/components/pages/Users/Users.jsx
+++ src/components/pages/Users/Users.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Users = () => {
     return (
-        <div >Users</div>
+        <div >
+            {/* 1. Parent Component For users Display */}
+            {/* 2. Child Components for  Users List
+                ¬ Child component User List --> SearchBar
+                ¬ Child component User List --> List of User
+                    ¬ Child Component User List --> User Object  */}
+        </div>
     )
 }
 
